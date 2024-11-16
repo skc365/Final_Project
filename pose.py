6b24ea84744f9a7d10697591df2cab6128af09de
suhyzxn <kimsuhyeon360@gmail.com>
2024-11-16 14:07:13 +0900

    pose.py

---
+++ pose.py
@@ -0,0 +1,33 @@
+import cv2
+import mediapipe as mp
+import numpy as np
+mp_drawing = mp.solutions.drawing_utils
+mp_pose = mp.solutions.pose
+
+cap = cv2.VideoCapture(0)
+with mp_pose.Pose(min_detection_confidence=0.5,
+                  min_tracking_confidence=0.5) as pose:
+    while cap.isOpened():
+        success, image = cap.read()
+
+        if not success:
+            continue
+
+        image = cv2.cvtColor(cv2.flip(image, 1), cv2.COLOR_BGR2RGB)
+
+        results = pose.process(image)
+
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+
+        mp_drawing.draw_landmarks(image, results.pose_landmarks,
+                                  mp_pose.POSE_CONNECTIONS,
+                                  mp_drawing.DrawingSpec(color=(245,117,66)),
+                                  mp_drawing.DrawingSpec(color=(245,66,230)))
+        
+        cv2.imshow('Mediapipe Feed', image)
+
+        if cv2.waitKey(1) == ord('q'):
+            break
+
+cap.release()
+cv2.destroyAllWindows()
