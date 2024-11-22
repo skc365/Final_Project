5e87a3da24cbd3d8bd4572ad27412f75e151f896
skc365 <kichan0220@gmail.com>
2024-11-22 21:47:23 +0900

    add calculate angle and classify pose1

--- pose.py
+++ pose.py
@@ -1,6 +1,8 @@
 import cv2
 import mediapipe as mp
 import numpy as np
+import math
+import matplotlib.pyplot as plt
 mp_drawing = mp.solutions.drawing_utils
 mp_pose = mp.solutions.pose
 
@@ -26,6 +28,53 @@ with mp_pose.Pose(min_detection_confidence=0.5,
                                   mp_pose.POSE_CONNECTIONS,
                                   mp_drawing.DrawingSpec(color=(245,117,66)),
                                   mp_drawing.DrawingSpec(color=(245,66,230)))
+
+        def calculateAngle(landmark1, landmark2, landmark3):
+
+            #landmark 1,2,3 정의
+            x1, y1, _ = landmark1
+            x2, y2, _ = landmark2
+            x3, y3, _ = landmark3
+
+            #각 계산
+            angle = math.degrees(math.atan2(y3 - y2, x3 - x2) - math.atan2(y1 - y2, x1 - x2))
+
+            #각이 음수이면, 360도 더해주기
+            if angle < 0:
+                angle += 360
+
+            return angle
+
+        def classifyPose(landmarks, output_image, display=False):
+
+            label = 'Unknown Pose'
+
+            color = (0, 0, 255)  # Red
+
+            left_elbow_angle = calculateAngle(landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value],
+                                              landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value],
+                                              landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value])
+
+            right_elbow_angle = calculateAngle(landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value],
+                                               landmarks[mp_pose.PoseLandmark.RIGHT_ELBOW.value],
+                                               landmarks[mp_pose.PoseLandmark.RIGHT_WRIST.value])
+
+            left_shoulder_angle = calculateAngle(landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value],
+                                                 landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value],
+                                                 landmarks[mp_pose.PoseLandmark.LEFT_HIP.value])
+
+            right_shoulder_angle = calculateAngle(landmarks[mp_pose.PoseLandmark.RIGHT_HIP.value],
+                                                 landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value],
+                                                 landmarks[mp_pose.PoseLandmark.RIGHT_ELBOW.value])
+
+            left_knee_angle = calculateAngle(landmarks[mp_pose.PoseLandmark.LEFT_HIP.value],
+                                             landmarks[mp_pose.PoseLandmark.LEFT_KNEE.value],
+                                             landmarks[mp_pose.PoseLandmark.LEFT_ANKLE.value])
+
+            right_knee_angle = calculateAngle(landmarks[mp_pose.PoseLandmark.RIGHT_HIP.value],
+                                              landmarks[mp_pose.PoseLandmark.RIGHT_KNEE.value],
+                                              landmarks[mp_pose.PoseLandmark.RIGHT_ANKLE.value])  #36:28
+
         
         cv2.imshow('Mediapipe Feed', image)
 
