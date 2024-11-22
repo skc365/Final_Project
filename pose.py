f496b73709efbedcd4d6a679140199ef8d7317dc
skc365 <kichan0220@gmail.com>
2024-11-22 21:15:23 +0900

    check and add url

--- pose.py
+++ pose.py
@@ -4,7 +4,10 @@ import numpy as np
 mp_drawing = mp.solutions.drawing_utils
 mp_pose = mp.solutions.pose
 
-cap = cv2.VideoCapture(0)
+url = ''
+
+cap = cv2.VideoCapture(url)
+
 with mp_pose.Pose(min_detection_confidence=0.5,
                   min_tracking_confidence=0.5) as pose:
     while cap.isOpened():
