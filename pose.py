9e4aa11836f87e6974dea0dba312a56d793250a1
skc365 <kichan0220@gmail.com>
2024-11-30 07:43:16 +0900

    modifying detectPose part

--- pose.py
+++ pose.py
@@ -4,63 +4,37 @@ import numpy as np
 from time import time
 import math
 import matplotlib.pyplot as plt
+
 mp_drawing = mp.solutions.drawing_utils
 mp_pose = mp.solutions.pose
+pose = mp.pose.Pose(static_image_mode = True, min_dectection_confidence=0.3, model_complexity=2)
 
 url = 'http://192.168.0.4:8080/video'
 
-cap = cv2.VideoCapture(url)
-
-with mp_pose.Pose(min_detection_confidence=0.5,
-                  min_tracking_confidence=0.5) as pose:
-    while cap.isOpened():
-        success, image = cap.read()
-
-        if not success:
-            continue
-
-        image = cv2.cvtColor(cv2.flip(image, 1), cv2.COLOR_BGR2RGB)
-
-        results = pose.process(image)
-
-        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-
-        mp_drawing.draw_landmarks(image, results.pose_landmarks,
-                                  mp_pose.POSE_CONNECTIONS,
-                                  mp_drawing.DrawingSpec(color=(245,117,66)),
-                                  mp_drawing.DrawingSpec(color=(245,66,230)))
-
-        def detectPose(image, pose, display=True):
-
-            output_image = image.copy()
-
-            imageRGB = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-
-            results = pose.process(imageRGB)
-
-            height, width, _ = image.shape
-
-            landmarks = []
-
-            if results.pose_landmarks:
-
-                mp_drawing.draw_landmarks(image=output_image,
-                                          landmark_list=results.pose_landmarks,
-                                          connections=mp_pose.POSE_CONNECTIONS)
-
-                for landmark in results.pose_landmarks.landmark:
-
-                    landmarks.append((int(landmark.x * width), int(landmark.y * height), (landmark.z * width)))
-
-            if display:
-                plt.figure(figsize=[22,22])
-                plt.subplot(121);plt.imshow(image[:,:,::-1]);plt.title("Original Image");plt.axis('off');
-                plt.subplot(122);plt.imshow(output_image[:,:,::-1]);plt.title("Output Image");plt.axis('off');
-
-                mp_drawing.plot_landmarks(results.pose_world_landmarks, mp_pose.POSE_CONNECTIONS)
-                
-            else:
-                return output_image, landmarks
+def detectPose(image, pose):
+    
+    # BGR -> RGB로 변환
+    imageRGB = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    
+    results = pose.process(imageRGB)
+    
+    # Retrieve the height and width of the input image.
+    height, width, _ = image.shape
+    
+    # 랜드마크 저장 리스트
+    landmarks = []
+    
+    # 랜드마크가 감지되면,
+    if results.pose_landmarks:
+    
+        # 랜드마크와 연결선 그리기
+        mp_drawing.draw_landmarks(image, landmark_list=results.pose_landmarks,
+                                  connections=mp_pose.POSE_CONNECTIONS)
+        
+        # 랜드마크 저장
+        for landmark in results.pose_landmarks.landmark:
+            landmarks.append((int(landmark.x * width), int(landmark.y * height),
+                                  (landmark.z * width)))
 
         def calculateAngle(landmark1, landmark2, landmark3):
 
